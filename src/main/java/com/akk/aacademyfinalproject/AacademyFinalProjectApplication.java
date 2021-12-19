5a468e9e79190761d1d8600848b4d394fd7d7a6d
akkrastevak <atanas.krastev@abv.bg>
2021-12-19 18:12:59 +0200

    Initial commit

---
+++ src/main/java/com/akk/aacademyfinalproject/AacademyFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akk.aacademyfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AacademyFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AacademyFinalProjectApplication.class, args);
+	}
+
+}
